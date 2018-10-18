35a9fbe59e8daf7518fe0a5a971d8f34ed768d31
patricioavero <patricioavero@users.noreply.github.com>
2018-10-18 19:45:48 -0300

    Update nagios_installer.sh

--- nagios_installer.sh
+++ nagios_installer.sh
@@ -7,8 +7,8 @@ TIME=`date +%T`
 PID=$$
 OS_NAME=`grep "^ID\b" /etc/os-release | cut -d "=" -f2 | cut -d "\"" -f2`
 LOG_FILE="./nagios-installer_${PID}_${DATE}-${TIME}.log"
-#PKGS2INSTALL="httpd php gcc glibc glibc-common make gd gd-devel net-snmp"
-PKGS2INSTALL="httpd"
+PKGS2INSTALL="httpd php gcc glibc glibc-common make gd gd-devel net-snmp"
+#PKGS2INSTALL="httpd"
 
 ## Functions
 
@@ -16,15 +16,15 @@ log2file () {
 	local STR=$1
 	local DATE=`date +%F`
 	local TIME=`date +%T`
-	
+
 	echo "${DATE}@${TIME} - ${STR}" | tee -a $LOG_FILE
 }
 
 install_pkg () {
 	local PKG=$1
-	
+
 	log2file "Installing the package ${PKG}"
-	
+
 	yum install -y ${PKG}
 }
 
@@ -32,10 +32,10 @@ check_installed_pkg () {
 	local PKG=$1
 
 	log2file "Checking if ${PKG} is installed"
-	
+
 	PKG_SEARCH=`rpmquery ${PKG}`
 	local EXIT_CODE=$?
-	
+
 	if [ ${EXIT_CODE} -eq 0 ]
 	then
 		log2file "The package ${PKG} is installed"
@@ -43,7 +43,7 @@ check_installed_pkg () {
 	else
 		log2file "The package ${PKG} is not installed"
 		return 11
-	fi	
+	fi
 
 
 }
@@ -85,7 +85,7 @@ then
 		log2file "Installing EPEL repo base on Centos 7 method"
 		install_pkg ${PKG_EPEL}
 		check_installed_pkg ${PKG_EPEL}
-	
+
 	elif [ "${OS_NAME}" == "rhel"]
 	then
 		log2file "Instsalling EPEL repo base on RHEL 7 method"
@@ -102,13 +102,13 @@ do
 	check_installed_pkg ${i}
 	EXIT_CODE=$?
 	#echo ${EXIT_CODE}
-	
+
 	if [ ${EXIT_CODE} -eq 11 ]
 	then
 		echo "Installing the Package: ${i}"
 		install_pkg ${i}
 	fi
-	
+
 	check_installed_pkg ${i}
 done
 ## [ END Packages Installation ] ##########
