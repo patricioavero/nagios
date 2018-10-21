39a1981d1cfc02d0a8beb0aba2ec2e150558c40d
patricioavero <patricio.avero@gmail.com>
2018-10-21 20:48:46 -0300

    Continuing

--- nagios_installer.sh
+++ nagios_installer.sh
@@ -12,8 +12,8 @@ PID=$$
 OS_NAME=`grep "^ID\b" /etc/os-release | cut -d "=" -f2 | cut -d "\"" -f2`
 LOG_FILE="./nagios-installer_${PID}_${DATE}-${TIME}.log"
 PKGS2INSTALL="httpd php gcc glibc glibc-common make gd gd-devel net-snmp"
-USER_NAGIOS="nagios"
-GROUP_NAGIOS="nagcmd"
+NAGIOS_USER="nagios"
+NAGIOS_GROUP="nagcmd"
 NAGIOS_SFW="https://assets.nagios.com/downloads/nagioscore/releases/nagios-4.4.2.tar.gz?__hstc=118811158.3adf54286ce2b00626c67842c7a6451e.1536434476127.1537402227302.1540145883596.3&__hssc=118811158.2.1540145883596&__hsfp=4002812518#_ga=2.248916295.138051336.1540145882-202040438.1536180874"
 NAGIOS_PLUGINS="https://nagios-plugins.org/download/nagios-plugins-2.2.1.tar.gz#_ga=2.248916295.138051336.1540145882-202040438.1536180874"
 DOWNLOAD_DIR="/var/tmp"
@@ -63,7 +63,7 @@ check_installed_pkg () {
 create_user () {
 	local USER=$1
 
-	useradd ${USER} -g ${GROUP_NAGIOS} 2>/dev/null
+	useradd ${USER} -g ${NAGIOS_GROUP} 2>/dev/null
 	local RESULT=$?
 
 	if [ ${RESULT} -eq 0 ]
@@ -152,8 +152,8 @@ done
 ## [ END Packages Installation ] ##########
 
 ## Users and Groups creation ##############
-create_group ${GROUP_NAGIOS}
-create_user ${USER_NAGIOS}
+create_group ${NAGIOS_GROUP}
+create_user ${NAGIOS_USER}
 ## [ END Users and groups creation ] ######
 
 ## Download the files needed for the installation 
@@ -168,7 +168,7 @@ tar -zxf ${DOWNLOAD_DIR}/nagios_plugins_2.2.1.tar.gz
 
 ## Install nagios and its plugin
 cd  ${DOWNLOAD_DIR}/nagios-4.4.2
-./configure --with-command-group=nagcmd && make all && make install && make install-init && make install-commandmode && make install-config && make install-webconf
+./configure --with-command-group=${NAGIOS_GROUP} && make all && make install && make install-init && make install-commandmode && make install-config && make install-webconf
 echo ${NAGIOS_ADMIN_PASSWORD} | htpasswd -b -s -c /usr/local/nagios/etc/htpasswd.users ${NAGIOS_ADMIN_USER}
 RESULT_NAGIOS_INSTALLATION=$?
 
@@ -176,16 +176,21 @@ if [ ${RESULT_NAGIOS_INSTALLATION} -eq 0 ]
 then
 	log2file "Nagios Sofware has been successfully installed."
 else
-	log2file "Something went wrong with the installation of Nagios"
+	log2file "Something went wrong with the installation of Nagios."
 fi
 
 cd ${DOWNLOAD_DIR}/nagios-plugins-2.2.1
-./configure --with-nagios-user=nagios --with-nagios-group=nagios && make && make install
+./configure --with-nagios-user=${NAGIOS_USER} --with-nagios-group=${NAGIOS_GROUP} && make && make install
 RESULT_PLUGIN_INSTALLATION=$?
 
 if [ ${RESULT_PLUGIN_INSTALLATION} -eq 0 ]
 then
-	log2file "PLugins has been successfully installed."
+	log2file "Plugins has been successfully installed."
 else
-	log2file "Something went wrong with the installation of Nagios Plugins"
+	log2file "Something went wrong with the installation of Nagios Plugins."
 fi
+## [ END of Nagios and PLugins installation ] ######
+
+## Start Services
+systemctl start http && systemctl enable httpd
+systemctl start nagios && systemctl enable nagios
